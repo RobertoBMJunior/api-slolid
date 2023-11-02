aa7b814487bd05da4ec422b7f05da161ba3243cf
Roberto Junior <robertojuniorazul@gmail.com>
2023-11-01 22:46:23 -0300

    create users, gyms and checkins tables

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
-import fastify from "fastify";
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Roberto Barbosa',
+    email: 'robertoazul@gmail.com',
+  },
+})
